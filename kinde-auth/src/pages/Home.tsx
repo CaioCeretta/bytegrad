b7240ec853484d66dda3a55f8cf432b6d33c4bef
andandtex <andandtex@gmail.com>
2024-08-31 19:20:27 -0300

    feat: end of lesson

--- kinde-auth/src/pages/Home.tsx
+++ kinde-auth/src/pages/Home.tsx
@@ -1,6 +1,9 @@
+import { useKindeAuth } from "@kinde-oss/kinde-auth-react";
 import ProtectedRoute from "./ProtectedRoute";
 
 function Home() {
+  const { logout, isAuthenticated, user } = useKindeAuth()
+
   return (
     <>
       <h1 className="text-5xl font-bold mb-3 text-red-500">Welcome to StockPrices</h1>
@@ -8,9 +11,31 @@ function Home() {
         In the dashboard you can see the stock prices of your favorite companies.
       </p>
 
-      <ProtectedRoute />
+      <div className="my-5">
+        <ProtectedRoute />
+      </div>
+
+      {
+        isAuthenticated && (
+          <div className="space-y-2 mt-10 mb-5">
+            <p>Your name: {user?.given_name}</p>
+            <p>Your email: {user?.email}</p>
+            <img src={user?.picture} alt="user" />
+
+            <pre>{JSON.stringify(user, null, 2)}</pre>
+
+            <button
+              onClick={() => logout()}
+              type="button"
+              className="bg-zinc-900 text-white px-3 py-2 rounded"
+            >
+              Log Out
+            </button>
+          </div>
+        )
+      }
     </>
   );
 }
- 
+
 export default Home;
--- kinde-auth/src/pages/ProtectedRoute.tsx
+++ kinde-auth/src/pages/ProtectedRoute.tsx
@@ -2,7 +2,7 @@ import { useKindeAuth } from "@kinde-oss/kinde-auth-react";
 import { Outlet } from "react-router-dom";
 
 export default function ProtectedRoute() {
-  const { isLoading, isAuthenticated, register, login } = useKindeAuth();
+  const { isLoading, isAuthenticated, login, register } = useKindeAuth();
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -12,18 +12,20 @@ export default function ProtectedRoute() {
     return (
       <div>
         <h1 className="text-3xl font-medium">Not authenticated</h1>
-        <button
-          onClick={() => login()}
-          className="bg-zinc-900 text-white px-3 py-2 rounded"
-        >
-          Login
-        </button>
-        <button
-          onClick={() => register()}
-          className="bg-zinc-900 text-white px-3 py-2 rounded"
-        >
-          Register
-        </button>
+        <div className="mt-2 flex items-center justify-center gap-3">
+          <button
+            onClick={() => login()}
+            className="bg-zinc-900 text-white px-3 py-2 rounded"
+          >
+            Login
+          </button>
+          <button
+            onClick={() => register()}
+            className="bg-zinc-900 text-white px-3 py-2 rounded"
+          >
+            Register
+          </button>
+        </div>
       </div>
     );
   }
