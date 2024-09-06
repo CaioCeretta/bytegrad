c90019f34c0e67f5c22f8b2c1ba3452fc1f70aa6
andandtex <andandtex@gmail.com>
2024-09-06 10:54:20 -0300

    echo: react app deletion and started a next for latest class

---
+++ hooks-mistakes/src/app/ContitionalRendering/index.tsx
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from 'react'
+
+export default function ConditionalRendering({ id }: {id: number}) {
+  if(!id) {
+    return 'No id provided'
+  }
+
+  const [something, setSomething] = useState('blabla')
+
+  useEffect(() => {
+
+  }, [something])
+
+
+  return (
+    <section>
+      {/* Product Card */}
+    </section>
+  )
+}
--- useEffect-mistakes/src/pages/Home.tsx
+++
@@ -1,8 +0,0 @@
-
-export default function Home() {
-  return (
-    <div className="w-100">
-      <p className="">Dashboard</p>
-    </div>
-  )
-}
