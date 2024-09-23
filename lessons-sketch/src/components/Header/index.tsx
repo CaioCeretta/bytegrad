08b19b893ee136c33ab422a76984e43c5d2c072d
andandtex <andandtex@gmail.com>
2024-09-23 14:37:05 -0300

    echo: Created the lessons sketch

---
+++ lessons-sketch/src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+
+function Header() {
+  return (
+    <header className="flex justify-center items-center">
+      <h1 className="text-2xl font-bold">Header Title</h1>
+    </header>
+  )
+}
+
+export { Header }
