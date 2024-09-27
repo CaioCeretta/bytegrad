a2bef3148aff758cef82e358c5c5ccb745c743e3
Caio <caioceretta@gmail.com>
2024-09-27 11:06:39 -0300

    feat: Start try catch lesson

---
+++ try-catch-ts/src/components/Header/Navbar/index.tsx
@@ -0,0 +1,19 @@
+import { ThemeToggler } from "../ThemeToggler";
+
+export default function Navbar() {
+  return (
+    <nav className="py-3 dark:bg-dark border-b-gray-200 border-b-2">
+      <div className="w-full max-w-screen-xl mx-auto flex
+      justify-between items-center">
+        <h1 className="text-3xl font-bold uppercase flex-1 text-gray-900">
+          Lesson<span className="text-gray-300">Name</span>
+        </h1>
+
+        <div className="flex flex-1 justify-end">
+          <ThemeToggler />
+        </div>
+
+      </div>
+    </nav>
+  )
+}
